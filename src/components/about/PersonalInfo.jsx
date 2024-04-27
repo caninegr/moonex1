7c861f3d7527ae2c62d0a3973fcab6ab1ad6b1b4
Vasileios Pappas <info@canine.gr>
2024-04-27 08:33:59 +0300

    first commit

--- src/components/about/PersonalInfo.jsx
+++ src/components/about/PersonalInfo.jsx
@@ -6,7 +6,7 @@ const PersonalInfo = () => {
       {/* <!--Animated Block--> */}
       <div className="ptf-animated-block" data-aos="fade" data-aos-delay="100">
         <h5 className="fz-14 text-uppercase has-3-color fw-normal">
-          Biography
+          Βιογραφικο
         </h5>
         {/* <!--Spacer--> */}
         <div className="ptf-spacer" style={{ "--ptf-xxl": "2.8125rem" }}></div>
@@ -14,8 +14,7 @@ const PersonalInfo = () => {
           className="fz-24 has-black-color has-secondary-font"
           style={{ maxWidth: "15.3125rem" }}
         >
-          Work for money and design for love! I’m Bruno, an UI/UX Interaction
-          Designer based in Poland.
+          Γεια σας, είμαι η Στέλλα, γνωστή και ως η "Φυσικοθεραπεύτρια των κατοικιδίων"!
         </p>
       </div>
       {/* End .ptf-animated-block */}
@@ -31,14 +30,14 @@ const PersonalInfo = () => {
 
       {/* <!--Animated Block--> */}
       <div className="ptf-animated-block" data-aos="fade" data-aos-delay="200">
-        <h5 className="fz-14 text-uppercase has-3-color fw-normal">Contact</h5>
+        <h5 className="fz-14 text-uppercase has-3-color fw-normal">Επικοινωνια</h5>
         {/* <!--Spacer--> */}
         <div className="ptf-spacer" style={{ "--ptf-xxl": "2.8125rem" }}></div>
         <p className="fz-24 has-black-color has-secondary-font">
-          Warsaw, Poland <br />
-          <a href="mailto:hello@brunoerdison.com">hello@brunoerdison.com</a>
+          Θεσσαλονίκη, Ελλάδα <br />
+          <a href="mailto:hello@brunoerdison.com">info@dogphysio.gr</a>
           <br />
-          <a href="tel:+6835688986">+68 3568 89 86</a>
+          <a href="tel:+6835688986">+30 6938 198989</a>
         </p>
       </div>
       {/* End .ptf-animated-block */}
@@ -54,14 +53,14 @@ const PersonalInfo = () => {
 
       {/* <!--Animated Block--> */}
       <div className="ptf-animated-block" data-aos="fade" data-aos-delay="300">
-        <h5 className="fz-14 text-uppercase has-3-color fw-normal">Services</h5>
+        <h5 className="fz-14 text-uppercase has-3-color fw-normal">Υπηρεσιες</h5>
         {/* <!--Spacer--> */}
         <div className="ptf-spacer" style={{ "--ptf-xxl": "2.8125rem" }}></div>
         <p className="fz-24 has-black-color has-secondary-font">
-          Website Design <br />
-          Mobile Application Design
+          Φυσικοθεραπεία <br />
+          Υδροθεραπεία
           <br />
-          Animation
+          Βελονισμός
         </p>
       </div>
       {/* End .ptf-animated-block */}
